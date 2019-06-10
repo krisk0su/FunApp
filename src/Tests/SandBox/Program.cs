b9e0cc51cacadfacd0bd68f23dea4eed2e38ae50
krisk0su <chrizkoof@gmail.com>
2019-06-10 22:28:01 +0200

    SandBox imp

--- src/Tests/SandBox/Program.cs
+++ src/Tests/SandBox/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.IO;
+using FunApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SandBox
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+          
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+
+                SandBoxCode(serviceProvider);
+            }
+        }
+
+        private static void SandBoxCode(IServiceProvider serviceProvider)
+        {
+            //TODO: code here
+        }
+
+        private static void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            serviceCollection.AddDbContext<FunAppContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
         }
     }
 }
